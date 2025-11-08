f6df37f3ae5c206fcbf51a9a2c86ee94b8690cb0
cforChobar <sidhuashman02@gmail.com>
2025-11-08 19:28:26 +0530

    Services page animations complete

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -3,11 +3,71 @@ import Button from "@/components/Button"
 import SectionHeading from "@/components/SectionHeading"
 import ServiceCard from "@/components/ServiceCard"
 import { servicesData } from "@/utils/data"
-import React from "react"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import React, { useRef } from "react"
+
+gsap.registerPlugin(useGSAP, ScrollTrigger)
 
 const Services = () => {
+    const popularServiceRef = useRef<HTMLDivElement>(null)
+    const serviceCaraouselRef = useRef<HTMLDivElement>(null)
+
+    useGSAP(() => {
+        // Popular Services Section Animations
+        const mm = gsap.matchMedia()
+        mm.add(
+            {
+                isDesktop: "(min-width: 1120px)",
+                isTablet: "(min-width: 768px) and (max-width: 1119px)",
+                isMobile: "(max-width: 767px)",
+            },
+            (context) => {
+                const { isDesktop, isTablet } = context.conditions as {
+                    isDesktop: boolean
+                    isTablet: boolean
+                }
+                const scrollWidth = () => {
+                    if (serviceCaraouselRef.current) {
+                        let width =
+                            serviceCaraouselRef.current.scrollWidth -
+                            window.innerWidth
+                        width += isDesktop ? 24 * 2 : isTablet ? 12 * 2 : 8 * 2
+                        return width
+                    } else {
+                        return 2000
+                    }
+                }
+                gsap.to(serviceCaraouselRef.current, {
+                    x: () => -scrollWidth(),
+                    scrollTrigger: {
+                        trigger: popularServiceRef.current,
+                        start: "top top",
+                        end: () => `+=${scrollWidth()}px`,
+                        scrub: 1,
+                        pin: true,
+                        pinType: "transform",
+                        pinSpacing: true,
+                        invalidateOnRefresh: true,
+                    },
+                })
+            }
+        )
+
+        // CTA Section Animations
+        gsap.to(".services-cta-hidden", {
+            opacity: 1,
+            ease: "sine.inOut",
+            scrollTrigger: {
+                trigger: ".services-cta-hidden",
+                start: "top 80%",
+            },
+        })
+    }, [])
     return (
         <main className="flex flex-col gap-32 overflow-hidden">
+            {/* add same kind of effects to all hero sections and I will think about that later so add other animations in mean time*/}
             <section className="my-container flex-gap h-screen min-h-[450px] w-full items-center justify-center">
                 <h1 className="hero-heading text-center">
                     <div>{servicesData.hero.heading1}</div>
@@ -17,7 +77,7 @@ const Services = () => {
                     {servicesData.hero.subHeading}
                 </p>
             </section>
-            <section className="flex-gap">
+            <section ref={popularServiceRef} className="flex-gap">
                 <div className="my-container">
                     <SectionHeading
                         headingOne={servicesData.popularServices.heading1}
@@ -25,12 +85,15 @@ const Services = () => {
                     />
                 </div>
                 <div className="caraousel overflow-hidden pl-2 md:pl-3 lg:pl-4">
-                    <div className="flex gap-2 overflow-visible md:gap-3 lg:gap-4">
+                    <div
+                        ref={serviceCaraouselRef}
+                        className="flex gap-2 overflow-visible md:gap-3 lg:gap-4"
+                    >
                         {servicesData.popularServices.servicesArray.map(
                             (service) => (
                                 <div
                                     key={service.id}
-                                    className="h-[600px] w-[320px] shrink-0 overflow-hidden md:h-[650px] md:w-[480px] lg:h-[calc(100vh-80px)] lg:min-h-[400px]"
+                                    className="h-[600px] w-[320px] shrink-0 overflow-hidden md:h-[650px] md:w-[480px] lg:h-[calc(100vh-128px)] lg:min-h-[400px]"
                                 >
                                     <ServiceCard
                                         title={service.name}
@@ -53,6 +116,7 @@ const Services = () => {
                             <ServiceCard
                                 title={service.name}
                                 img={service.img}
+                                data-speed="auto"
                             />
                         </div>
                     ))}
@@ -63,10 +127,12 @@ const Services = () => {
                     headingOne={servicesData.ctaSection.heading}
                     classes="hero-heading text-center md:w-10/12 lg:w-3/4"
                 />
-                <p className="paragraph-text text-center text-gray-500 md:w-10/12 lg:w-3/4">
+                <p className="paragraph-text services-cta-hidden text-center text-gray-500 opacity-0 md:w-10/12 lg:w-3/4">
                     {servicesData.ctaSection.subHeading}
                 </p>
-                <Button title={servicesData.ctaSection.cta} />
+                <div className="services-cta-hidden opacity-0">
+                    <Button title={servicesData.ctaSection.cta} />
+                </div>
             </section>
         </main>
     )
--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -1,23 +1,70 @@
-import React from "react"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import React, { useRef } from "react"
 import { twMerge } from "tailwind-merge"
 
 interface SectionHeadingProps {
-	headingOne: string
-	headingTwo?: string
-	classes? : string
+    headingOne: string
+    headingTwo?: string
+    classes?: string
 }
 
+gsap.registerPlugin(useGSAP, ScrollTrigger)
+
 const SectionHeading = ({
-	headingOne,
-	headingTwo = "",
-	classes = "",
+    headingOne,
+    headingTwo = "",
+    classes = "",
 }: SectionHeadingProps) => {
-	return (
-		<h1 className={twMerge("section-heading", classes)}>
-			<div>{headingOne}</div>
-			{headingTwo && <div>{headingTwo}</div>}
-		</h1>
-	)
+    const mainRef = useRef<HTMLHeadingElement>(null)
+    const headingOneRef = useRef<HTMLDivElement>(null)
+    const headingTwoRef = useRef<HTMLDivElement>(null)
+    const tlRef = useRef<GSAPTimeline>(null)
+    useGSAP(() => {
+        if (tlRef.current) tlRef.current.kill()
+        tlRef.current = gsap.timeline({
+            defaults: {
+                duration: 0.5,
+                ease: "sine.inOut",
+            },
+            scrollTrigger: {
+                trigger: mainRef.current,
+                start: "top 80%",
+            },
+            onComplete: () => {
+                gsap.to(mainRef.current, {
+                    overflow: "visible",
+                })
+            },
+        })
+        tlRef.current.to(headingOneRef.current, {
+            y: 0,
+        })
+        if (headingTwoRef.current)
+            tlRef.current.to(
+                headingTwoRef.current,
+                {
+                    y: 0,
+                },
+                "<"
+            )
+    }, [])
+    return (
+        <h1
+            ref={mainRef}
+            className={twMerge("section-heading overflow-hidden", classes)}
+        >
+            <div ref={headingOneRef} className="translate-y-full">
+                {headingOne}
+            </div>
+            {headingTwo && (
+                <div ref={headingTwoRef} className="translate-y-full">
+                    {headingTwo}
+                </div>
+            )}
+        </h1>
+    )
 }
 
 export default SectionHeading
